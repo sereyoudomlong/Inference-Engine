3c8501b1375fc5c88d6d425452f3f4e336c2ed9b
sereyoudomlong <sereyoudom78@gmail.com>
2021-05-03 16:37:24 +1000

    Update iEngine.java

--- src/Engine/iEngine.java
+++ src/Engine/iEngine.java
@@ -8,11 +8,12 @@ public class iEngine {
 
     public List<String> _kb = new ArrayList<String>();
     public String _query;
-    public List<String> _var;
+    public List<String> _var = new ArrayList<String>();
 
     public iEngine(String KB, String Query){
         _query = Query.replaceAll(" ", "");
         InitKB(KB);
+        InitVar();
     }
 
     public void InitKB (String KB){
@@ -25,20 +26,44 @@ public class iEngine {
         }
     }
 
+    public void InitVar(){
+        // goes through all element in _kb
+        for (int i = 0; i < _kb.size(); i++){
+            //split them by '=>' e.g. a&b=>p3 would become [a&b,p3]
+            String[] temp1 = _kb.get(i).split("=>");
+            // for each string after split
+            for (String var1 : temp1){
+                // split them again if they have '&' e.g. a&b would become [a, b]
+                String[] temp2 = var1.split("&");
+                // for each string after the second split
+                for (String var2 : temp2){
+                    // check if the string is in list of variable
+                    if (_var.contains(var2))
+                        continue;               // if true skip
+                    else
+                        _var.add(var2);         //else add it in
+                }
+            }
+        }
+    }
+
+
+
 
     // testing function
     public void printInfo(){
         System.out.println("Knowledge Base: ");
-        try{
-            for (int i = 0; i < _kb.size(); i++){
-                System.out.println(_kb.get(i));
-            }
-        }catch(NullPointerException ex){
-            System.out.println("error");
+        for (int i = 0; i < _kb.size(); i++){
+            System.out.println(_kb.get(i));
         }
 
         System.out.println("Query: ");
         System.out.println(_query);
+
+        System.out.println("Variables: ");
+        for (int i = 0; i < _var.size(); i++){
+            System.out.println(_var.get(i));
+        }
     }
 
 }
